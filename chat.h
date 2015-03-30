0b7b34ad53f6fc57cfb50f7110d85fbaf67479bc
themuntdregger <themuntdregger@gmail.com>
2015-03-30 01:09:07 +0100

    test commit
    Signed-off-by: themuntdregger <themuntdregger@gmail.com>

--- chat.h
+++ chat.h
@@ -15,7 +15,7 @@
 
 	You should have received a copy of the GNU General Public License
 	along with unoff_server_4.  If not, see <http://www.gnu.org/licenses/>.
-*******************************************************************************************************************/
+******************************************************************************************************************/
 
 #ifndef CHAT_H_INCLUDED
 #define CHAT_H_INCLUDED
